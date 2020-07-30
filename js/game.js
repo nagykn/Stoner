f57231f5102a7c052452043f3cded0db9c519ba5
nagykn <nagykristofnandor@outlook.hu>
2020-07-30 21:23:37 +0200

    fixes

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-const stands = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+var stands = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 
 startBtn = document.querySelector("#start");
 div = document.querySelector("#stands");
